ebeb284a40ee8ff8f4a685225aa06cdfc3befa67
freezingSummer <38682200@qq.com>
2020-11-17 02:53:08 -0900

    fix SegFault

--- temp/test.c
+++
@@ -1,8 +0,0 @@
-#include "include/symbolTable.h"
-
-int main() {
-    symbolTable *newTable = createTable();
-    clearTable(newTable);
-
-    insert(newTable, createNodeEntry("1", createEntryValue(1)));
-}
